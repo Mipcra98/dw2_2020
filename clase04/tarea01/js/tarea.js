30d866ab65ec5a6a63e914b9798543788f2d1c51
Willian Führ <willianfuhr27041998@gmail.com>
2020-03-30 21:22:47 -0400

    ActualizaciónNº1 30-03-2020

--- clase04/tarea01/js/tarea.js
+++ clase04/tarea01/js/tarea.js
@@ -1,6 +1,16 @@
-var Lista_de_numeros = [0,1,2,5,9,8,7,3,6,4,10,11,15,19,18,12,17,13,16,14,20];
-Lista_de_numeros.sort(ordenar)
-function ordenar(a, b){
-  return a - b;
+var Origen=[];
+function generador() {
+  var a = (Math.random(0,1))*100;
+  return Math.floor(a);
 }
-console.log(Lista_de_numeros);
+for (var i = 0; i<10; i++){
+  Origen[i]=generador();
+}
+console.log("Origen = "+Origen);
+var Destino = [];
+Destino = Origen;
+function ordenar(a,b){
+  return a-b;
+}
+Destino.sort(ordenar)
+console.log("Destino = "+Destino);
