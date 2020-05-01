02bac5a5bfc2a3e9a7314c16b7e6fd0767e672f3
BuildTools <unconfigured@null.spigotmc.org>
2020-05-01 18:03:40 -0400

    Tarea Nº10 - Clase 08

--- clase08/cerrar.php
+++ clase08/cerrar.php
@@ -14,15 +14,24 @@ session_destroy();
    <body class="bg-secondary">
      <div class="container bg-dark text-white rounded-lg">
        <h1 style="margin-top:2rem">Sesión Cerrada</h1>
-       <h3>Lista de personas registradas durante la sesión</h3>
-       <ol>
-       <?php
-       $personas = $_SESSION['personas'];
-       foreach ($personas as $key => $value) {
-           echo "<li>".$value['nombre']." ".$value['apellido']." , con fecha de nacimiento el: ".$value['fecha']."</li>";
-       }
-       ?>
-       </ol>
+       <h3>Lista de personas registradas durante la sesión</h3><table class="table">
+            <table class="table table-striped table-dark">
+             <thead class="thead-light">
+               <tr>
+                 <th scope="col">Nombre</th>
+                 <th scope="col">Apellido</th>
+                 <th scope="col">Fecha de Nacimiento</th>
+               </tr>
+             </thead>
+             <tbody>
+               <?php
+               $personas = $_SESSION['personas'];
+               foreach ($personas as $key => $value) {
+                   echo "<tr><th scope='row'>".$value['nombre']."</th><td>".$value['apellido']."</td><td>".$value['fecha']."</td></tr>";
+               }
+               ?>
+             </tbody>
+           </table>
        <a class="btn btn-primary" href="formulario.php" style="margin-bottom:1rem">Volver al Formulario</a>
      </div>
    </body>
